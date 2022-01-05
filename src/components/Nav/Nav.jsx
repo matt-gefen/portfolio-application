9782ae579f6c58ec2c8425de2883462326266037
mattgefen <matt.gefen@gmail.com>
2022-01-05 14:06:54 -0700

    adding more links to nav bar and initial format

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,6 +5,9 @@ const Nav = (props) => {
   return (
     <nav className='nav-bar'>
       <NavLink to='/'>Home</NavLink>
+      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/projects'>Projects</NavLink>
+      <NavLink to='/contact'>Contact</NavLink>
     </nav>
   )
 }
