3b02270dadd177cd5e11006ff325e0ad5966743f
mattgefen <matt.gefen@gmail.com>
2022-01-05 14:09:19 -0700

    stubbing Landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './styles/App.css'
+import {Routes, Route} from 'react-router-dom'
 
 import Nav from './components/Nav/Nav';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Routes>
+        <Route path="/" element={<Landing />}/>
+      </Routes>
     </div>
   );
 }
