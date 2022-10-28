b8d9ddd50b64cb5b57c296a2f09069426d1ba4e4
mattgefen <matt.gefen@gmail.com>
2022-10-28 12:05:47 -0600

    adding experience tabs

---
+++ src/components/Landing/Experience/PrimaryTab.jsx
@@ -0,0 +1,11 @@
+import {React} from 'react'
+
+const PrimaryTab = ({title, index, setSelectedTab, selectedTab}) => {
+    return (
+        <div className={`primary-tab ${selectedTab === index ? 'selected' : ''}`} onClick={() => setSelectedTab(index)}>
+            {title}
+        </div>
+    )
+}
+
+export default PrimaryTab
