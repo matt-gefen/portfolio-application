c12b97ebd4a3953248ff5a2814bd70fb3a9b1591
mattgefen <matt.gefen@gmail.com>
2022-10-28 12:05:25 -0600

    adding primary experience tabs

--- src/components/Landing/Experience/Experience.jsx
+++ src/components/Landing/Experience/Experience.jsx
@@ -1,12 +1,31 @@
-import {React} from 'react'
+import {React, useState} from 'react'
+import PrimaryTab from './PrimaryTab'
+import SubTab from './SubTab';
+
+import { map } from 'lodash';
 
 const Experience = (props) => {
+    const experience = require('./data/experience.json')
+    // const [tabs, setSelectedTab] = useState(0);
+    const [selectedTab, setSelectedTab] = useState(0);
+    const tabs = map(experience.tabs, (element, index) => {
+        return <PrimaryTab title={element.tabName} index={index} setSelectedTab={setSelectedTab} selectedTab={selectedTab}/>
+    })
+    console.log(tabs)
+
     return (
         <div className='experience' id='experience'>
             <div className='section-header'>
                 Experience
             </div>
             <div className='experience-main-section'>
+                <div className='primary-tabs'>
+                    {tabs}
+                </div>
+                <div>
+                    {experience.tabs[selectedTab].subtabs[0].title}
+                </div>
+
             </div>
         </div>
     )
